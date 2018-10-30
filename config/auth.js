b100e4731e1ba90daea03aa6d6914acdae49c6f4
jayrajk <jkapadia23@gmail.com>
2018-10-30 18:50:04 +0530

    working on routes

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports= {
+    'googleAuth' : {
+        'clientID'      : '760720754167-nldv5q1nii25grjrkro5h0rtaohgujkm.apps.googleusercontent.com',
+        'clientSecret'  : 'rM-h0B6a0ENAyFoIZq3HnR5R',
+        'callbackURL'   : 'http://localhost:4005/auth/google/callback'
+    }
+}

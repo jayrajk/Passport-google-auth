ecbc5531d51eac35358edbfda638cca3e86141bd
jayrajk <jkapadia23@gmail.com>
2018-10-31 12:11:00 +0530

    completed google Oauth2.0

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,20 @@ module.exports = function(app, passport) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
+    // route for showing the login page
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // route for showing the signup page
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
     // route for showing the profile page
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
@@ -17,6 +31,20 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
     // GOOGLE ROUTES =======================
     app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
